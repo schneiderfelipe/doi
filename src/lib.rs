5e2f731d54ed69986c5c52f905cd423edb2da584
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-04-28 16:31:00 -0300

    docs: improve

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,26 @@
 //! A strongly-typed library for working
 //! with
-//! [Digital object identifiers](https://en.wikipedia.org/wiki/Digital_object_identifier).
+//! [Digital object identifiers](https://en.wikipedia.org/wiki/Digital_object_identifier)
 //! (DOIs).
 
 use nutype::nutype;
 use once_cell::sync::Lazy;
 use url::Url;
 
-/// Prefix URL for a
-/// [Digital object identifier](https://en.wikipedia.org/wiki/Digital_object_identifier).
+/// Prefix URL for retrieving information about
+/// [Digital object identifiers](https://en.wikipedia.org/wiki/Digital_object_identifier)
+/// (DOIs).
 static URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://doi.org/").expect("URL should be parseable"));
 
-/// Namespace of a
-/// [Digital object identifier](https://en.wikipedia.org/wiki/Digital_object_identifier).
+/// Namespace of
+/// [Digital object identifiers](https://en.wikipedia.org/wiki/Digital_object_identifier)
+/// (DOIs).
 const NAMESPACE: &str = "10.";
 
 /// A
-/// [Digital object identifier](https://en.wikipedia.org/wiki/Digital_object_identifier).
+/// [Digital object identifier](https://en.wikipedia.org/wiki/Digital_object_identifier)
+/// (DOI).
 #[nutype(
     sanitize(trim, lowercase, with = sanitize_doi)
     validate(not_empty, with = validate_doi)
@@ -26,13 +29,22 @@ const NAMESPACE: &str = "10.";
 #[derive(Display)]
 pub struct Doi(String);
 
+/// Sanitize a
+/// [Digital object identifier](https://en.wikipedia.org/wiki/Digital_object_identifier)
+/// (DOI).
 #[allow(clippy::map_unwrap_or)]
 fn sanitize_doi(doi: String) -> String {
-    [URL.as_str(), "doi:"].into_iter().fold(doi, |doi, prefix| {
-        doi.strip_prefix(prefix).map(Into::into).unwrap_or(doi)
-    })
+    [URL.as_str(), "doi:"]
+        .into_iter()
+        .fold(doi.as_str(), |doi, prefix| {
+            doi.strip_prefix(prefix).unwrap_or(doi)
+        })
+        .into()
 }
 
+/// Validate a
+/// [Digital object identifier](https://en.wikipedia.org/wiki/Digital_object_identifier)
+/// (DOI).
 fn validate_doi(doi: &str) -> bool {
     doi.starts_with(NAMESPACE) && doi.contains('/')
 }
