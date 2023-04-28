482315599b5c97ce955aee13edc1dbf55eb3e774
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-04-28 12:17:43 -0300

    feat: use clap derive API

--- src/bin/doi2bib.rs
+++ src/bin/doi2bib.rs
@@ -1,38 +1,27 @@
 use std::process;
 
-use clap::App;
-use clap::AppSettings;
-use clap::Arg;
+use clap::Parser;
 
-fn print_version() {
-    println!("doi2bib 1.1");
+/// Retrieve BibTeX data for a given DOI.
+#[derive(Parser)]
+#[command(author, version, about, arg_required_else_help(true))]
+struct Cli {
+    /// DOI to retrieve BibTeX data for.
+    doi: Vec<Doi>,
 }
 
+type Doi = String;
+
 fn main() {
-    let matches = App::new("doi2bib")
-        .version("1.1")
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .about("Retrieves BibTeX data for a given DOI")
-        .arg(
-            Arg::with_name("doi")
-                .multiple(true)
-                .required(true)
-                .help("The DOI to retrieve BibTeX data for"),
-        )
-        .get_matches();
+    let cli = Cli::parse();
 
-    if matches.is_present("version") {
-        print_version();
-        return;
-    }
-
-    for doi in matches.values_of("doi").unwrap() {
+    for doi in cli.doi {
         // We do some encoding as needed.
         let doi = doi.replace('+', "%2B");
 
         // We retrieve the data from https://doi.org/
         match reqwest::blocking::Client::new()
-            .get(&format!("https://doi.org/{}", doi))
+            .get(&format!("https://doi.org/{doi}"))
             .header(reqwest::header::ACCEPT, "text/bibliography; style=bibtex")
             .send()
         {
@@ -44,10 +33,10 @@ fn main() {
                     .replace("}, ", "},\n  ")
                     .replace(",\n  ", ", ")
                     .replace("}}", "}\n}\n");
-                println!("{}", bibtex_data);
+                println!("{bibtex_data}");
             }
             Err(e) => {
-                eprintln!("Error retrieving DOI {}: {}", doi, e);
+                eprintln!("Error retrieving DOI {doi}: {e}");
                 process::exit(1);
             }
         }
